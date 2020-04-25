14174c98dfc322e465bae45c3fc573939c677afd
Josue Villanueva <idjevm@gmail.com>
2020-04-25 12:25:12 -0400

    working backend api deployed to VERCEL with MEN Stack

---
+++ README.md
@@ -0,0 +1,1 @@
+## Deploy a Basic Node App using Now
---
+++ config/db.js
@@ -0,0 +1,20 @@
+// config/db.js
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            process.env.MONGO_ADDRESS,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        ); 
+        console.log('MongoDB is Connected...');
+    } catch(err) {
+        console.error(err.message); 
+        process.exit(1);
+    }
+}; 
+
+module.exports = connectDB; 
