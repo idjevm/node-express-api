0344deb95a845ba81c147b9b7281a1f8b5efd38e
Josue Villanueva <idjevm@gmail.com>
2020-04-26 03:12:38 -0400

    add cors headers and updated routes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // app.js
 require('dotenv').config(); // load env vars from .env file
 const express = require('express');
-const connectDB = require('./config/db'); 
-const cors = require('cors'); 
+const connectDB = require('./config/db');
+const cors = require('cors');
 
 // Setting PORT 
 const PORT = process.env.PORT;
@@ -20,13 +20,37 @@ connectDB();
 // cors
 app.use(cors());
 
+// const allowedOrigins = ['http://localhost:3000',
+//                       'http://yourapp.com'];
+
+// app.use(
+//     cors({
+//         origin: function (origin, callback) {
+//             // allow requests with no origin 
+//             // (like mobile apps or curl requests)
+//             if (!origin) return callback(null, true);
+//             if (allowedOrigins.indexOf(origin) === -1) {
+//                 var msg = 'The CORS policy for this site does not ' +
+//                     'allow access from the specified Origin.';
+//                 return callback(new Error(msg), false);
+//             }
+//             return callback(null, true);
+//         }
+//     }));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Init Middleware
-app.use(express.json({ extended: false })); 
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello JV!'));
 
 // use Routes
-app.use('/books', bookRoute);
+app.use('/api/books', bookRoute);
 // ...more routes here...
 
-app.listen(PORT,  () => console.log(`Server is now running on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is now running on port: ${PORT}`));
